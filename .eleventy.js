6bd2e0cf3da20f4e6df5e8d72b0a255c6a8e7edf
Drew Jones <drew.jones@centrica.com>
2022-10-21 22:38:48 +0100

    add assets

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addWatchTarget("./src/styles.css");
 
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/styles.css");
+
   return {
     dir: {
       input: "src",
